54c5566139e72ffc879861d17471c3663f169fe1
Khaled Silva <khaled.silva17@gmail.com>
2026-08-09 22:09:31 -0300

    adicionada atualização de filmes

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,6 @@ app.post("/movies", async (req, res) => {
         req.body;
 
     try {
-
         const movieWithSameTitle = await prisma.movie.findFirst({
             where: {
                 title: { equals: title, mode: "insensitive" },
@@ -52,6 +51,41 @@ app.post("/movies", async (req, res) => {
     }
 
     res.status(201).send();
+});
+
+app.put("/movies/:id", async (req, res) => {
+    // pegar o id do registro que vai ser atualizado
+    const id = Number(req.params.id);
+
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: { id },
+        });
+
+        if (!movie) {
+            return res.status(404).send({ message: "Filme não encontrado" });
+        }
+
+        const data = { ...req.body };
+        data.release_date = data.release_date
+            ? new Date(data.release_date)
+            : undefined;
+
+        // pegar os dados do filme que será atualizado e atualizar ele no prisma
+        await prisma.movie.update({
+            where: {
+                id,
+            },
+            data: data,
+        });
+    } catch (error) {
+        return res
+            .status(500)
+            .send({ message: "Falha ao atualizar o registro" });
+    }
+
+    //retornar o status correto informando que o filme foi atualizado
+    res.status(200).send();
 });
 
 app.listen(port, () => {
