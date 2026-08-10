c517dae2b0e43b539cbf146d58e5becbe62ceaa9
Khaled Silva <khaled.silva17@gmail.com>
2026-08-09 21:13:12 -0300

    adicionada correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -25,6 +25,19 @@ app.post("/movies", async (req, res) => {
         req.body;
 
     try {
+
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: {
+                title: { equals: title, mode: "insensitive" },
+            },
+        });
+
+        if (movieWithSameTitle) {
+            return res
+                .status(409)
+                .send({ message: "Já existe um filme com esse título" });
+        }
+
         await prisma.movie.create({
             data: {
                 title,
@@ -35,7 +48,7 @@ app.post("/movies", async (req, res) => {
             },
         });
     } catch (error) {
-        return res.status(500).send({message: "Falha ao cadastrar um filme"});
+        return res.status(500).send({ message: "Falha ao cadastrar um filme" });
     }
 
     res.status(201).send();
